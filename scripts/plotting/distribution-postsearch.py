f4605ff7b6398c98526eec67624491d83b4cdc3c
Tianyi Gu <2008tygu@gmail.com>
2019-07-29 16:22:13 -0400

    update plot code for original and post search to be use same bin width

--- scripts/plotting/distribution-postsearch.py
+++ scripts/plotting/distribution-postsearch.py
@@ -1,85 +1,143 @@
+from collections import OrderedDict
+from collections import defaultdict
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import pandas as pd
-import json
 import seaborn as sns
 import numpy as np
-import re
-from os import listdir
-from collections import defaultdict
-from collections import OrderedDict
 
-def makeHistrogram(h, hslistpair, fileDir):
-    sns.set(rc={'figure.figsize': (11, 8), 'font.size': 26, 'text.color': 'black'})
 
-    ax_ps = sns.distplot(np.array(hslistpair[1]), label="Post Search Belief", kde=False, norm_hist=True, hist_kws={"histtype": "step", "linewidth": 1,"alpha": 1, "color": "b"})
-    ax_ps.axvline(np.array(hslistpair[1]).mean(), color='b', linewidth=4, ymin = 0.45, ymax=0.55)
+def make_histrogram(h_value, hslistpair, file_dir):
+    '''Make histrogram given h value and two hstart
 
-    ax_or = sns.distplot(np.array(hslistpair[0]), label="Original Belief", kde=False, norm_hist=True, hist_kws={"histtype": "step", "linewidth": 1,"alpha": 1, "color": "r"})
-    ax_or.axvline(np.array(hslistpair[0]).mean(), color='r', linewidth=4, ymin = 0.45, ymax=0.55)
+    keyword arguments:
+    h_value -- h value
+    hslistpair -- hs values
+    file_dir -- output dir
+    '''
+    sns.set(rc={
+        'figure.figsize': (11, 8),
+        'font.size': 26,
+        'text.color': 'black'
+    })
+
+    iqr1 = np.subtract(*np.percentile(np.array(hslistpair[0]), [75, 25]))
+    sample_number = len(hslistpair[0])
+    bin_width = 2 * iqr1 / (sample_number**(1 / 3.0))
+    bin_num = None if bin_width == 0.0 else int(
+        (max(hslistpair[0]) - min(hslistpair[0])) / bin_width)
+    print("bin_width:", bin_width)
+    print("bin_num:", bin_num)
+
+    print("iqr: ", iqr1)
+    print("n: ", sample_number)
+
+    ax_ps = sns.distplot(np.array(hslistpair[1]),
+                         label="Post Search Belief",
+                         kde=False,
+                         norm_hist=True,
+                         hist_kws={
+                             "histtype": "step",
+                             "linewidth": 1,
+                             "alpha": 1,
+                             "color": "b"
+                         },
+                         bins=bin_num)
+    ax_ps.axvline(np.array(hslistpair[1]).mean(),
+                  color='b',
+                  linewidth=4,
+                  ymin=0.45,
+                  ymax=0.55)
+
+    ax_or = sns.distplot(np.array(hslistpair[0]),
+                         label="Original Belief",
+                         kde=False,
+                         norm_hist=True,
+                         hist_kws={
+                             "histtype": "step",
+                             "linewidth": 1,
+                             "alpha": 1,
+                             "color": "r"
+                         },
+                         bins=bin_num)
+    ax_or.axvline(np.array(hslistpair[0]).mean(),
+                  color='r',
+                  linewidth=4,
+                  ymin=0.45,
+                  ymax=0.55)
 
     #ax.tick_params(colors='black', labelsize=12, rotation=90)
     # ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
     # ax.set_xscale('log')
     # plt.ylabel("density", color='black', fontsize=18)
     plt.xlabel("h*", color='black', fontsize=18)
-    plt.title("h="+ h )
+    plt.title("h=" + h_value)
     plt.legend()
-    plt.savefig(fileDir+"h"+h.zfill(5)+".eps", bbox_inches="tight", pad_inches=0)
+    plt.savefig(file_dir + "h" + h_value.zfill(5) + ".eps",
+                bbox_inches="tight",
+                pad_inches=0)
     plt.close()
     plt.clf()
     plt.cla()
     return
 
-def parseDataFile(fname):
+
+def parse_data_file(fname):
+    ''' parse data from a file
+
+    keyworkd arguments:
+    fname -- input file
+    '''
     h_collection = defaultdict(list)
 
-    f = open(fname, "r")
+    input_file = open(fname, "r")
 
-    for line in f:
+    for line in input_file:
         values = line.split()
-        h = values[0]
+        h_value = values[0]
         count = (int)(values[1])
         cur = 2
         while cur < len(values):
-           curCount = (int)(values[cur+1]) 
-           while curCount > 0:
-               h_collection[h].append((float)(values[cur]))
-               curCount=curCount-1
-           cur = cur + 2 
-        assert len(h_collection[h]) == count
+            cur_count = (int)(values[cur + 1])
+            while cur_count > 0:
+                h_collection[h_value].append((float)(values[cur]))
+                cur_count = cur_count - 1
+            cur = cur + 2
+        assert len(h_collection[h_value]) == count
 
     return h_collection
 
-def merge2map(m1, m2):
-    m = defaultdict(list)
-    for h, l in m1.iteritems():
-        assert(h in m2)
-        m[h].append(l)
-        m[h].append(m2[h])
-        m2.pop(h)
-    assert (len(m2)==0)
-    return m
+
+def merge2map(map_1, map_2):
+    '''merge two h->hs maps into one map h->[hs,hs]
+
+    '''
+    ret_map = defaultdict(list)
+    for h_value, hs_value in map_1.iteritems():
+        assert h_value in map_2
+        ret_map[h_value].append(hs_value)
+        ret_map[h_value].append(map_2[h_value])
+        map_2.pop(h_value)
+    assert not map_2
+    return ret_map
 
 
 # Hard coded data file
-# beliefFile="../../../results/SlidingTilePuzzle/sampleData/inverse-statSummary-20-0.1-200-backup.txt"
-# postsearchbeliefFile="../../../results/SlidingTilePuzzle/sampleData/inverse-statSummary-20-0.1-200-postSearch.txt"
-beliefFile="../../../results/SlidingTilePuzzle/sampleData/uniform-statSummary.txt"
-postsearchbeliefFile="../../../results/SlidingTilePuzzle/sampleData/uniform-statSummary-postSearch.txt"
+FILE_BELIEF="../../../results/SlidingTilePuzzle/sampleData/inverse-statSummary-20-0.1-200-backup.txt"
+FILE_PS_BELIEF="../../../results/SlidingTilePuzzle/sampleData/inverse-statSummary-20-0.1-200-postSearch.txt"
+# FILE_BELIEF = "../../../results/SlidingTilePuzzle/sampleData/uniform-statSummary.txt"
+# FILE_PS_BELIEF = "../../../results/SlidingTilePuzzle/sampleData/uniform-statSummary-postSearch.txt"
 
-print("reading in data...")
-belief_origin = parseDataFile(beliefFile)
-belief_ps = parseDataFile(postsearchbeliefFile)
+print "reading in data..."
+H_TO_HS = parse_data_file(FILE_BELIEF)
+H_TO_PSHS = parse_data_file(FILE_PS_BELIEF)
 
-belief_together=merge2map(belief_origin, belief_ps)
+H_TO_HSS_MAP = merge2map(H_TO_HS, H_TO_PSHS)
 
-print("plotting...")
+print "plotting..."
 
-od = OrderedDict(sorted(belief_together.items()))
-# plotDir ="../../../plots/hist/inverse/20-0.1-200/"
-plotDir ="../../../plots/hist/uniform/postSearch/"
+OD = OrderedDict(sorted(H_TO_HSS_MAP.items()))
+PLOT_DIR ="../../../plots/hist/inverse/20-0.1-200/"
+# PLOT_DIR = "../../../plots/hist/uniform/postSearch/"
 
-for h, pair in od.items():
-    if len(pair[0])>1 :
-        makeHistrogram(h,pair,plotDir)
+for h, pair in OD.items():
+    if len(pair[0]) > 1:
+        make_histrogram(h, pair, PLOT_DIR)
